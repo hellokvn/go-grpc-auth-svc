64657fa597c1c103a3dab65c30391db65f9aaa92
Kevin <hellokevinvogel@gmail.com>
2022-03-29 15:09:21 +0200

    chore(): rm gorm.model

--- pkg/models/auth.go
+++ pkg/models/auth.go
@@ -1,9 +1,6 @@
 package models
 
-import "gorm.io/gorm"
-
 type Auth struct {
-	gorm.Model
 	Id       int64  `json:"id" gorm:"primaryKey"`
 	Email    string `json:"email"`
 	Password string `json:"password"`
